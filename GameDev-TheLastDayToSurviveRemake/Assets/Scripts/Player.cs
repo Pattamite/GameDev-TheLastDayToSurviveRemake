b8d9644ce8a1f20568f905374b16900d63edfdd0
Pattnatt <jittapat007@gmail.com>
2018-05-11 15:55:14 +0700

    Player can rotate to mouse position

---
+++ GameDev-TheLastDayToSurviveRemake/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Camera mainCamera;
+
+    void Start () {
+        mainCamera = Camera.main;
+
+    }
+
+    void Update () {
+        UpdateRotation();
+
+    }
+
+    private void UpdateRotation () {
+        Vector3 mousePosWorld = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 currentPosition = transform.position;
+        Vector3 currentRotation = transform.eulerAngles;
+
+        float deltaX = mousePosWorld.x - currentPosition.x;
+        float deltaY = mousePosWorld.y - currentPosition.y;
+
+        float rotationAngle = Mathf.Atan2(deltaY, deltaX) / Mathf.PI * 180f;
+        transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, rotationAngle);
+    }
+}
