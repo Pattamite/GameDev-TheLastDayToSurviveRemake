caceaff79a1ae50456a40000adb6711541189e06
Pattnatt <jittapat007@gmail.com>
2018-05-12 10:35:23 +0700

    Player can move

--- GameDev-TheLastDayToSurviveRemake/Assets/Scripts/Player.cs
+++ GameDev-TheLastDayToSurviveRemake/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public GameObject playerCharacter;
+    public float movementSpeed = 1f;
+
     private Camera mainCamera;
 
     void Start () {
@@ -12,19 +15,30 @@ public class Player : MonoBehaviour {
     }
 
     void Update () {
+        UpdateMovement();
         UpdateRotation();
-
     }
 
     private void UpdateRotation () {
         Vector3 mousePosWorld = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         Vector3 currentPosition = transform.position;
-        Vector3 currentRotation = transform.eulerAngles;
+        Vector3 currentRotation = playerCharacter.transform.eulerAngles;
 
         float deltaX = mousePosWorld.x - currentPosition.x;
         float deltaY = mousePosWorld.y - currentPosition.y;
 
         float rotationAngle = Mathf.Atan2(deltaY, deltaX) / Mathf.PI * 180f;
-        transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, rotationAngle);
+        playerCharacter.transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, rotationAngle);
+    }
+
+    private void UpdateMovement () {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, moveY, 0);
+        if (movement.magnitude > 1) movement = movement.normalized;
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+        //transform.position += movement * movementSpeed;
     }
 }
