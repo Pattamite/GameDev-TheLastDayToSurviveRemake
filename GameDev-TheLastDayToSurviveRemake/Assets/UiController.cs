059d2b79f95e5e03ce6ceb78199a9d67b3254e79
Pattnatt <jittapat007@gmail.com>
2018-05-18 11:20:40 +0700

    Add Ammo Text

---
+++ GameDev-TheLastDayToSurviveRemake/Assets/UiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour {
+    public Text ammoText;
+    public Text reservedAmmoText;
+
+    private Player player;
+    private AssaultRifle assaultRifle;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindObjectOfType<Player>();
+        assaultRifle = GameObject.FindObjectOfType<AssaultRifle>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ammoText.text = assaultRifle.currentAmmo.ToString();
+        reservedAmmoText.text = assaultRifle.pocketAmmo.ToString();
+
+    }
+}
