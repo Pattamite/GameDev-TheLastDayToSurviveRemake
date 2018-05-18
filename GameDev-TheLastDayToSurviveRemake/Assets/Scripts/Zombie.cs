3537be189c0ba0d653cc780de751c1633ee8e283
Pattnatt <jittapat007@gmail.com>
2018-05-18 15:52:12 +0700

    Zombie Movement

---
+++ GameDev-TheLastDayToSurviveRemake/Assets/Scripts/Zombie.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+    public Animator animator;
+    public GameObject zombieSprite;
+    public float speed = 1.5f;
+    public int maxHealth = 100;
+
+    private Player Player;
+    private bool isDead;
+    private int currentHealth;
+
+	void Start () {
+        isDead = false;
+        currentHealth = maxHealth;
+        Player = GameObject.FindObjectOfType<Player>();
+
+    }
+	
+	void Update () {
+        if(!isDead) UpdateRotationAndPosition();
+	}
+
+    private void UpdateRotationAndPosition () {
+        Vector3 playerPos = Player.transform.position;
+        Vector3 currentPosition = transform.position;
+        Vector3 currentRotation = zombieSprite.transform.eulerAngles;
+
+        float deltaX = playerPos.x - currentPosition.x;
+        float deltaY = playerPos.y - currentPosition.y;
+
+        float rotationAngle = Mathf.Atan2(deltaY, deltaX) / Mathf.PI * 180f;
+        zombieSprite.transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, rotationAngle);
+
+        Vector3 movement = new Vector3(deltaX, deltaY, 0);
+        transform.Translate(movement.normalized * speed * Time.deltaTime);
+    }
+
+
+    public void GotHit(int damage) {
+        currentHealth -= damage;
+        if (currentHealth <= 0) Dead();
+    }
+
+    private void Dead () {
+        isDead = true;
+        animator.SetBool("isDead", true);
+    }
+
+    public void Kill () {
+        Destroy(gameObject);
+    }
+}
