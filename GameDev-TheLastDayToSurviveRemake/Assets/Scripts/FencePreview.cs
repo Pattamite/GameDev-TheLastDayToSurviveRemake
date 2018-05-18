0f680fc41bb49d3354b0402c38fbe62ec6d8f858
Pattnatt <jittapat007@gmail.com>
2018-05-18 09:45:24 +0700

    Add FenceReview

---
+++ GameDev-TheLastDayToSurviveRemake/Assets/Scripts/FencePreview.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FencePreview : MonoBehaviour {
+    public SpriteRenderer spriteRenderer;
+    public Sprite okSprite;
+    public Sprite notOkSprite;
+    public bool isDeployable {get; private set;}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D (Collider2D collision) {
+        if (collision.gameObject.GetComponent<Fence>()) {
+            isDeployable = false;
+            spriteRenderer.sprite = notOkSprite;
+        }
+    }
+
+    private void OnTriggerExit2D (Collider2D collision) {
+        if (collision.gameObject.GetComponent<Fence>()) {
+            isDeployable = true;
+            spriteRenderer.sprite = okSprite;
+        }
+    }
+}
